17148b5182abc0a75dfa1738bf08a50d6cd067af
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-28 23:11:42 +0800

    Write lines

--- Line Writer.py
+++ Line Writer.py
@@ -1,3 +1,11 @@
 # Open a file 
 file = open("mylife.txt", "w")
 
+# Write lines
+while True:
+    line = input("Enter line: ")
+    file.write(line + "\n")
+    choice = input("Are there more lines? y/n : ")
+    if choice == "n" :
+        break
+    
